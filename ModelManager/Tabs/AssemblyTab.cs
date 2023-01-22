216ac91fcd94f86659b66be6737cd68cbe27062e
Kevin Shortall <Kevin.Shortall@welfare.ie>
2023-01-22 17:10:25 +0000

    Fix app

---
+++ ModelManager/Tabs/AssemblyTab.cs
@@ -0,0 +1,36 @@
+﻿using AssemblyAnalyser;
+using ModelManager.Core;
+using ModelManager.Types;
+using ModelManager.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace ModelManager.Tabs
+{
+    public class AssemblyTab : AbstractServiceTab
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Assembly";
+            }
+        }
+
+        public void AnalyseAssembly()
+        {
+            var assemblyPath = @"";
+            var interfaces = new List<Type>();
+            var analyser = new Analyser();
+            var assemblySpec = new AssemblySpec(Assembly.LoadFrom(assemblyPath));
+            assemblySpec.AnalyseAsync(analyser);
+            var types = analyser.Types();
+        }
+    }
+}
